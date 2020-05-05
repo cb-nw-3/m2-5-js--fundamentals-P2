22786570e14f9ffd4e786f9b242701247dcd8937
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-05-04 22:27:31 -0400

    completed 6-1-4

--- workshop/6-1-test-cases/problem4.js
+++ workshop/6-1-test-cases/problem4.js
@@ -1,5 +1,3 @@
-let verifyEquals = require('../../assets/verify-equals');
-
 // Problem 4
 // ---------
 // Write a function that returns the letter at the specified position in the
@@ -10,14 +8,22 @@ let verifyEquals = require('../../assets/verify-equals');
 // getLetterAtIndex("", 4);      // undefined
 // getLetterAtIndex("abc", 0);   // a
 
-function getLetterAtIndex(arr) {
-  // Your code here
+function getLetterAtIndex(str, index) {
+  const letter = str.charAt(index);
+  if (letter === "") return undefined;
+  return letter;
 }
 
 // Add 6 more test cases
-expect(getLetterAtIndex('hello', 4), 'o');
-expect(getLetterAtIndex('goodbye', 0), 'g');
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("goodbye", 0), "g");
 
+expect(getLetterAtIndex("", 1), undefined);
+expect(getLetterAtIndex("hola", 2), "l");
+expect(getLetterAtIndex("adios", 4), "s");
+expect(getLetterAtIndex("bonjour", 3), "j");
+expect(getLetterAtIndex("demain ", 7), undefined);
+expect(getLetterAtIndex("toyota", 1), "o");
 /**
  * -------------------------------------------------------------------
  * ⚠️ No changes necessary below. ⚠️
@@ -25,7 +31,7 @@ expect(getLetterAtIndex('goodbye', 0), 'g');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
