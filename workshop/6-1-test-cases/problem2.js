78ea7e90340d5018d09fd0473652b2a41f789207
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-05-04 20:39:26 -0400

    completed 6-1-2

--- workshop/6-1-test-cases/problem2.js
+++ workshop/6-1-test-cases/problem2.js
@@ -8,14 +8,26 @@
 // - If it's a number, return `undefined`
 
 function lastCharacter(str) {
-  /* Your code here */
+  let lastChar = str.charAt(str.length - 1);
+  if (lastChar === "") {
+    return undefined;
+  }
+  if (typeof lastChar === "number") {
+    return undefined;
+  }
+  return lastChar;
 }
 
 // Step 2
 // You're given 1 test case. Add 4 more, making sure to cover all of the
 // conditions specified above (don't forget empty string and number!!)
 
-expect(lastCharacter('max'), 'x');
+expect(lastCharacter("max"), "x");
+
+expect(lastCharacter("Caller1"), "1");
+expect(lastCharacter(" "), " ");
+expect(lastCharacter("Yogurt"), "t");
+expect(lastCharacter("creativity"), "y");
 
 // Add 4 more test cases here!
 // 🌠 NOTE 🌠
@@ -34,7 +46,7 @@ expect(lastCharacter('max'), 'x');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
