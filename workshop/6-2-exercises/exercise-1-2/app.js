5b50cf2e9c0512f0e564d4b5394f83ba2e3e1a3d
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-05-07 18:58:05 -0400

    partly completed 6-2-1-2 then used sol'n to finish

--- workshop/6-2-exercises/exercise-1-2/app.js
+++ workshop/6-2-exercises/exercise-1-2/app.js
@@ -7,6 +7,7 @@
 // button two turns 'crimson'
 // button three turns 'lightblue'
 // button four starts shaking... (animation provided in styles.css)
+let buttonList = document.querySelector("#btn-list");
 
 // STEP 2
 // This time the buttons should toggle.
@@ -20,3 +21,42 @@
 // Because we are now going to work with the opacity and background of the buttons,
 // they need to be set here. So that they are referencable via the the style attribute.
 // initialize the background color for all of the buttons in here. (for loop)
+
+for (let id = 1; id <= 4; id++) {
+  let initialButton = document.getElementById(`btn-${id}`);
+
+  initialButton.style.opacity = "100";
+  initialButton.style.backgroundColor = "gold";
+}
+
+function toggleColor(id, color) {
+  let buttonElem = document.getElementById(id);
+  let currentColor = buttonElem.style.backgroundColor;
+
+  buttonElem.style.backgroundColor = currentColor === "gold" ? color : "gold";
+}
+
+function animateButtons(event) {
+  let buttonId = event.target.id;
+  let buttonElem = document.getElementById(buttonId);
+
+  switch (buttonId) {
+    case "btn-1":
+      buttonElem.style.opacity =
+        buttonElem.style.opacity === "100" ? "0" : "100";
+      break;
+    case "btn-2":
+      toggleColor(buttonId, "crimson");
+      break;
+    case "btn-3":
+      toggleColor(buttonId, "lightblue");
+      break;
+    case "btn-4":
+      buttonElem.classList.add("jitters");
+      break;
+    default:
+      break;
+  }
+}
+
+buttonList.addEventListener("click", animateButtons);
