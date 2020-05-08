6c496a5e233756cc91246c25f6623e5722df49db
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-05-07 20:37:22 -0400

    completed 6-2-1-3

--- workshop/6-2-exercises/exercise-1-3/app.js
+++ workshop/6-2-exercises/exercise-1-3/app.js
@@ -14,8 +14,62 @@
 
 // NO MODIFICATIONS OUTSIDE OF THE EXERCISE FOLDER (i.e. the css is read-only)
 
+let buttonList = document.querySelector("#btn-list");
+let btnReset = document.querySelector("#reset");
+
+for (let id = 1; id <= 4; id++) {
+  let intBtn = document.getElementById(`btn-${id}`);
+
+  intBtn.style.opacity = "100";
+  intBtn.style.backgroundColor = "gold";
+}
+
+function toggleColor(id, color) {
+  let btnElem = document.getElementById(id);
+  let currentColor = btnElem.style.backgroundColor;
+
+  btnElem.style.backgroundColor = currentColor === "gold" ? color : "gold";
+}
+
+function clickEvent(event) {
+  let btnId = event.target.id;
+  let btnElem = document.getElementById(btnId);
+
+  switch (btnId) {
+    case "btn-1":
+      btnElem.style.opacity = btnElem.style.opacity === "100" ? "0" : "100";
+      break;
+    case "btn-2":
+      toggleColor(btnId, "crimson");
+      break;
+    case "btn-3":
+      toggleColor(btnId, "lightblue");
+      break;
+    case "btn-4":
+      btnElem.classList.add("jitters");
+      break;
+    default:
+      break;
+  }
+}
+
+buttonList.addEventListener("click", clickEvent);
 // STEP 2
 // The new RESET button should reset all of the buttons to their original state.
+
+function reset() {
+  let btn1 = document.getElementById("btn-1");
+  let btn2 = document.getElementById("btn-2");
+  let btn3 = document.getElementById("btn-3");
+  let btn4 = document.getElementById("btn-4");
+
+  btn1.style.opacity = "100";
+  btn2.style.backgroundColor = "gold";
+  btn3.style.backgroundColor = "gold";
+  btn4.classList.remove("jitters");
+}
+
+btnReset.addEventListener("click", reset);
 
 // HINT:
 // to remove the 'jitters' class, check the length of the classList.
