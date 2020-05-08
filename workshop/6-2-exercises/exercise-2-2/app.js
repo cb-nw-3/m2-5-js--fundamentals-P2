51192e17811aabec7cee699bd549746f9556fd61
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-05-08 19:06:27 -0400

    completed 6-2-2-2

--- workshop/6-2-exercises/exercise-2-2/app.js
+++ workshop/6-2-exercises/exercise-2-2/app.js
@@ -13,3 +13,40 @@
 // - one event listener per input
 
 // If you need extra hints, see somebody... :)
+let board = document.getElementById("board");
+let rowsInput = document.getElementById("rows-input");
+let columnsInput = document.getElementById("columns-input");
+
+board.style.width = "600px";
+board.style.height = "600px";
+
+let rows, columns;
+
+function generateBoard() {
+  if (rows > 0 && columns > 0) {
+    board.innerHTML = "";
+
+    board.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+
+    for (let i = 0; i < rows * columns; i++) {
+      const cell = document.createElement("div");
+      cell.id = `cell-${i}`;
+      cell.classList.add("cell");
+      board.appendChild(cell);
+    }
+  }
+}
+
+function makeRows(event) {
+  rows = event.target.value;
+  generateBoard();
+}
+
+function makeColumns(event) {
+  columns = event.target.value;
+  generateBoard();
+}
+
+rowsInput.addEventListener("blur", makeRows);
+columnsInput.addEventListener("blur", makeColumns);
