820f252f949f25215376f8b50905ba732d331b31
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-05-07 21:51:32 -0400

    completed 6-2-2-1

--- workshop/6-2-exercises/exercise-2-1/app.js
+++ workshop/6-2-exercises/exercise-2-1/app.js
@@ -11,3 +11,15 @@
 // - the cells should all be square.
 // - cells should have a class of 'cell'
 // - cells should have and id of 'cell-#'
+let board = document.getElementById("board");
+board.style.width = "600px";
+board.style.height = "600px";
+board.style.gridTemplateColumns = "repeat(10, 1fr)";
+board.style.gridTemplateRows = "repeat(10, 1fr)";
+
+for (let i = 0; i < 100; i++) {
+  const cell = document.createElement("div");
+  cell.id = `cell-${i}`;
+  cell.classList.add("cell");
+  board.appendChild(cell);
+}
