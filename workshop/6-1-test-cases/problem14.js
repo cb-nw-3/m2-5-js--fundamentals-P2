bb96dc86f57dd17a9337971f2cdde238eb80e6f5
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-05-09 15:55:29 -0400

    exercise 14, 1.1, 1.2, 1.3

--- workshop/6-1-test-cases/problem14.js
+++ workshop/6-1-test-cases/problem14.js
@@ -8,7 +8,32 @@
 // - To represent a newline character, you can use "\n".
 
 function wrapAfter40Chars(str) {
-  // Your code here!
+  // check if argument is a string
+  if (typeof str === 'string') {
+    // split the string in an array
+    let strArr = str.split('');
+    // initialize the new string
+    let newStr = '';
+    // loop through the array
+    for (let i = 0; i < strArr.length; i++) {
+      // do something every fortieth character
+      if (i % 40 === 0 && i !== 0) {
+        // add enter
+        newStr += '\n';
+        // check if character is a space
+        if (strArr[i] !== ' ') {
+          // add it to the new string if not
+          newStr += strArr[i]
+        }
+      } else {
+        // add rest of character
+        newStr += strArr[i];
+      }
+    }
+    return newStr
+  } else {
+    return undefined
+  }
 }
 
 // Test case 1: No space after newline
@@ -28,6 +53,22 @@ expect(
 );
 
 // Add 4 more test cases
+expect(
+  wrapAfter40Chars(
+    'My favourite thing about cats is'
+  ),
+  'My favourite thing about cats is'
+);
+
+expect(wrapAfter40Chars(123), undefined);
+expect(wrapAfter40Chars([123]), undefined);
+
+// // // // expect(
+// // // //   wrapAfter40Chars(
+// // // //     'My favourite thing about cats is all the things.My favourite thing about cats is all the things.My favourite thing about cats is all the things.My favourite thing about cats is all the things.'
+// // // //   ),
+// // // //   'My favourite thing about cats is all the\nthings.My favourite thing about cats is \nall the things.My favourite thing about \ncats is all the things.My favourite thi\nng about cats is all the things.'
+// // // // );
 
 /*
  * -------------------------------------------------------------------
--- workshop/6-2-exercises/exercise-1-2/app.js
+++ workshop/6-2-exercises/exercise-1-2/app.js
@@ -1,6 +1,5 @@
 // Exercise 1.2
 // ----------
-
 // STEP 1
 // Reimplement the functionality of 1.1 (Try to NOT look at your previous solution)
 // button one disappears
@@ -20,3 +19,62 @@
 // Because we are now going to work with the opacity and background of the buttons,
 // they need to be set here. So that they are referencable via the the style attribute.
 // initialize the background color for all of the buttons in here. (for loop)
+
+// select buttons
+let firstButton = document.querySelector('#btn-1');
+let secondButton = document.querySelector('#btn-2');
+let thirdButton = document.querySelector('#btn-3');
+let fourthButton = document.querySelector('#btn-4');
+
+// set the attributes
+firstButton.setAttribute('style', 'opacity: 1; background: gold;');
+secondButton.setAttribute('style', 'background: gold;');
+thirdButton.setAttribute('style', 'background: gold;');
+fourthButton.setAttribute('style', 'background: gold;');
+
+// put da buttons in array
+let buttonsArray = [firstButton, secondButton, thirdButton, fourthButton];
+
+// loop through array
+buttonsArray.forEach(elem => {
+  // add click event
+  elem.addEventListener('click', function() {
+    switch (this) {
+      // first case
+      case firstButton:
+        // if visible change opcatiy to 0
+        if (firstButton.style.opacity === '1') {
+          firstButton.style.opacity = '0';
+        // if invisible change opacity to 1
+        } else if (firstButton.style.opacity === '0') {
+          firstButton.style.opacity = '1';
+        }
+        break;
+      // second case
+      case secondButton:
+        // if gold change to crimson
+        if (secondButton.style.backgroundColor === 'gold') {
+          secondButton.style.backgroundColor = 'crimson';
+        // if crimson change to gold
+        } else if (secondButton.style.backgroundColor === 'crimson') {
+          secondButton.style.backgroundColor = 'gold';
+        }
+        break;
+      // third case
+      case thirdButton:
+        // if gold change to lightblue
+        if (thirdButton.style.backgroundColor === 'gold') {
+          thirdButton.style.backgroundColor = 'lightblue';
+        // if light blue change to gold
+        } else if (thirdButton.style.backgroundColor === 'lightblue') {
+          thirdButton.style.backgroundColor = 'gold';
+        }
+        break;
+      // fourth case
+      case fourthButton:
+        // toggle on and off the animation
+        fourthButton.classList.toggle('jitters');
+        break;
+    }
+  })
+})
