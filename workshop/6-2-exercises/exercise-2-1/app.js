cef6b6fb52e17f6589ab440a3ccc736c7b458966
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-05-08 21:49:47 -0400

    ex6.2 completed 2-1 and 2-2

--- workshop/6-2-exercises/exercise-2-1/app.js
+++ workshop/6-2-exercises/exercise-2-1/app.js
@@ -10,4 +10,26 @@
 // - set a number of cells per row;
 // - the cells should all be square.
 // - cells should have a class of 'cell'
-// - cells should have and id of 'cell-#'
+// - cells should have and id of 'cell-#''
+
+const boardElement = document.getElementById('board');
+
+//set board size
+boardElement.style.height = '600px';
+boardElement.style.width = '600px';
+
+//set number of rows and columns
+const ROWS = 10;
+const COLUMNS = 10;
+
+//add grid cells
+boardElement.style.gridTemplateRows = `repeat(${ROWS},auto)`;
+boardElement.style.gridTemplateColumns = `repeat(${COLUMNS},auto)`;
+
+//add elements into grid
+for(let i = 0; i < ROWS * COLUMNS; i++ ) {
+  let cell = document.createElement('div');
+  cell.classList.add('cell');
+  cell.setAttribute('id', `cell-${i+1}`);
+  boardElement.appendChild(cell);
+}
