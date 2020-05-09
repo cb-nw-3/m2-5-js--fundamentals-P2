c134d242ad1c8b4b068896705abb157ffdb2d1d9
donnyesq <adonis@darmas.ca>
2020-05-09 10:31:50 -0400

    Exercise 14 complete

--- workshop/6-1-test-cases/problem14.js
+++ workshop/6-1-test-cases/problem14.js
@@ -9,7 +9,18 @@
 
 function wrapAfter40Chars(str) {
   let arr = str.split("");
-  for (let i = 0; i < arr.length; i++) {}
+  let result = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 40 === 0) {
+      result += "\n";
+      if (arr[i] !== " ") {
+        result += arr[i];
+      }
+    } else {
+      result += arr[i];
+    }
+  }
+  return result;
 }
 
 // Test case 1: No space after newline
@@ -24,6 +35,34 @@ expect(
 expect(
   wrapAfter40Chars("My favourite thing about cats is all the things."),
   "My favourite thing about cats is all the\nthings."
+);
+
+expect(
+  wrapAfter40Chars(
+    "You were the reason they stuck around, you're old enough now."
+  ),
+  "You were the reason they stuck around bu\nt you're old enough now."
+);
+
+expect(
+  wrapAfter40Chars(
+    "There will still be a lecture starting at 9, but it will be very short."
+  ),
+  "There will still be a lecture starting a\nt 9, but it will be very short."
+);
+
+expect(
+  wrapAfter40Chars(
+    "When there are people in the queue, we will try to keep each consultation to less than 20 minutes."
+  ),
+  "When there are people in the queue, we w\nill try to keep each consultation to les\ns than 20 minutes."
+);
+
+expect(
+  wrapAfter40Chars(
+    "Welcome to the Zoom Legal Center. The information provided here is for Zoom users who have questions about our terms, policies, and compliance."
+  ),
+  "Welcome to the Zoom Legal Center. The in\nformation provided here is for Zoom user\ns who have questions about our terms, po\nlicies, and compliance."
 );
 
 // Add 4 more test cases
