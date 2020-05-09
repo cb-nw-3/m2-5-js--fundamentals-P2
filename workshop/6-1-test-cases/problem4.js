6d1b1568929c75ad499b21ae772bdc8495a83e63
n4dz <nadzamini@gmail.com>
2020-05-09 19:13:04 -0400

    problem 4 done

--- workshop/6-1-test-cases/problem4.js
+++ workshop/6-1-test-cases/problem4.js
@@ -8,13 +8,36 @@
 // getLetterAtIndex(["", 4]);      // undefined
 // getLetterAtIndex(["abc", 0]);   // a
 
+/*
+1. créer une fonction
+2. retourner une position identifier d'un string
+3. si la position n'existe pas retourner undefined
+*\
+
+
+
+/*
+  si la position n'existe pas
+    retourner undefined
+  sinon
+    retourner la lettre identifiée.
+*/
+
 function getLetterAtIndex(arr) {
-  // Your code here
+  if (!arr[0].charAt(arr[1])) {
+    return undefined;
+  } else {
+    return arr[0].charAt(arr[1]);
+  }
 }
 
 // Add 6 more test cases
-expect(getLetterAtIndex(['hello', 4]), 'o');
-expect(getLetterAtIndex(['goodbye', 0]), 'g');
+expect(getLetterAtIndex(["hello", 4]), "o");
+expect(getLetterAtIndex(["goodbye", 0]), "g");
+expect(getLetterAtIndex(["kitkat", 40]), undefined);
+expect(getLetterAtIndex(["apple", 2]), "p");
+expect(getLetterAtIndex(["StarWars", 8]), undefined);
+expect(getLetterAtIndex(["Banana", 5]), "a");
 
 /**
  * -------------------------------------------------------------------
@@ -23,10 +46,8 @@ expect(getLetterAtIndex(['goodbye', 0]), 'g');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
-    console.log(
-      `⛔️ Expected “${result}” to equal “${value}”`
-    );
+    console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
 }
