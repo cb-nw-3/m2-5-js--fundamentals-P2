e6988ea1996fbd078b8175d76469777143a66c2e
rogerlam <rogerlam@wylco.org>
2020-05-09 16:20:47 -0400

    finish all except optional

--- workshop/6-2-exercises/exercise-2-1/app.js
+++ workshop/6-2-exercises/exercise-2-1/app.js
@@ -11,3 +11,26 @@
 // - the cells should all be square.
 // - cells should have a class of 'cell'
 // - cells should have and id of 'cell-#'
+
+let board = document.querySelector("#board");
+board.style.width = "600px";
+board.style.height = "600px";
+
+// this should create a grid of 15 by 15 squares.
+// it doesn't seem to want to create empty squares.
+
+for (let i = 0; i <= 14; i++) {
+    let row = document.createElement("div");
+    for (let j = 0; j <= 14; j++) {
+        let cell = document.createElement("span");
+        cell.style.width = "35px";
+        cell.style.height = "35px";
+        cell.style.margin = "0 2px"
+        cell.style.background = "green";
+        cell.style.display = "inline-block";
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("id", "cell-" + i + j);
+        row.appendChild(cell);
+    }
+    board.appendChild(row);
+}
--- workshop/6-2-exercises/exercise-2-2/app.js
+++ workshop/6-2-exercises/exercise-2-2/app.js
@@ -13,3 +13,30 @@
 // - one event listener per input
 
 // If you need extra hints, see somebody... :)
+
+let board = document.querySelector("#board");
+board.style.width = "600px";
+board.style.height = "600px";
+
+function generateBoard(x, y) {
+    document.querySelector("#board").innerHTML = "";
+
+    board.style.gridTemplateRows = `repeat(${x}, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${y}, 1fr)`;
+
+    for (let i = 0; i < x * y; i++) {
+        let cell = document.createElement("span");
+        cell.setAttribute("id", "cell-" + i);
+        cell.classList.add("cell");
+        board.appendChild(cell);
+    }
+}
+
+
+document.querySelector('#rows-input').addEventListener("blur", function () {
+    generateBoard(document.querySelector('#rows-input').value, document.querySelector('#columns-input').value);
+});
+
+document.querySelector('#columns-input').addEventListener("blur", function () {
+    generateBoard(document.querySelector('#rows-input').value, document.querySelector('#columns-input').value);
+});
