6870f61373e73e1d27363699f95900a613985dcc
n4dz <nadzamini@gmail.com>
2020-05-09 23:50:56 -0400

    exrcise 1-1 & 1-2 done

--- workshop/6-2-exercises/exercise-1-1/app.js
+++ workshop/6-2-exercises/exercise-1-1/app.js
@@ -14,3 +14,26 @@
 // Guidelines
 // - write only one event listener
 // - use a switch statement
+
+/*
+1. btn-1 = disparait
+2. btn-2 = crimson
+3. btn-3 = lightblue
+4. btn-4 = shaking
+*/
+
+let btnList = document.getElementById("btn-list");
+btnList.addEventListener("click", function (event) {
+  let buttonIdSelection = event.target.id;
+  if (buttonIdSelection === "btn-1") {
+    document.getElementById(buttonIdSelection).style.opacity = 0;
+  } else if (buttonIdSelection === "btn-2") {
+    document.getElementById(buttonIdSelection).style.backgroundColor =
+      "crimson";
+  } else if (buttonIdSelection === "btn-3") {
+    document.getElementById(buttonIdSelection).style.backgroundColor =
+      "lightblue";
+  } else if (buttonIdSelection === "btn-4") {
+    document.getElementById(buttonIdSelection).classList.add("jitters");
+  }
+});
--- workshop/6-2-exercises/exercise-1-2/app.js
+++ workshop/6-2-exercises/exercise-1-2/app.js
@@ -20,3 +20,35 @@
 // Because we are now going to work with the opacity and background of the buttons,
 // they need to be set here. So that they are referencable via the the style attribute.
 // initialize the background color for all of the buttons in here. (for loop)
+
+let btnList = document.getElementById("btn-list");
+btnList.addEventListener("click", function (event) {
+  let buttonIdSelection = event.target.id;
+  if (buttonIdSelection === "btn-1") {
+    if (document.getElementById(buttonIdSelection).style.opacity == 0) {
+      document.getElementById(buttonIdSelection).style.opacity = 100;
+    } else {
+      document.getElementById(buttonIdSelection).style.opacity = 0;
+    }
+  } else if (buttonIdSelection === "btn-2") {
+    if (
+      document.getElementById(buttonIdSelection).style.backgroundColor == "gold"
+    ) {
+      document.getElementById(buttonIdSelection).style.backgroundColor =
+        "crimson";
+    } else {
+      document.getElementById(buttonIdSelection).style.backgroundColor = "gold";
+    }
+  } else if (buttonIdSelection === "btn-3") {
+    if (
+      document.getElementById(buttonIdSelection).style.backgroundColor == "gold"
+    ) {
+      document.getElementById(buttonIdSelection).style.backgroundColor =
+        "lightblue";
+    } else {
+      document.getElementById(buttonIdSelection).style.backgroundColor = "gold";
+    }
+  } else if (buttonIdSelection === "btn-4") {
+    document.getElementById(buttonIdSelection).classList.toggle("jitters");
+  }
+});
