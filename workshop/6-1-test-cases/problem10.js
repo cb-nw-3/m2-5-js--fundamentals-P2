b45c421132b77d3ecb2cd49d918d223f82ffaef9
n4dz <nadzamini@gmail.com>
2020-05-09 21:32:54 -0400

    problem 10 done

--- workshop/6-1-test-cases/problem10.js
+++ workshop/6-1-test-cases/problem10.js
@@ -10,15 +10,37 @@
 //  - Iterate over each word, calling your "capitalize word" function
 //  - Join the results into a string
 
+/*
+1. Mettre les string en array
+2. Regarder chaque élément du array
+3. Modifier le premier caractere pour le mettre en majuscule
+4. Mettre le resultat en string
+*/
+
 function makeIntoTitle(sentence) {
-  // Your code here
+  let result = "";
+  let final = "";
+  const myArray = sentence.split(" ");
+  for (let index = 0; index < myArray.length; index++) {
+    result =
+      myArray[index].charAt(0).toUpperCase() +
+      myArray[index].slice(1).toLowerCase();
+    result = result + " ";
+    final = final + result;
+  }
+  return final.slice(0, -1);
 }
 
 // Add 6 total (5 more)
 expect(
-  makeIntoTitle('the longest road is a great song'),
-  'The Longest Road Is A Great Song'
+  makeIntoTitle("the longest road is a great song"),
+  "The Longest Road Is A Great Song"
 );
+expect(makeIntoTitle("i like movie"), "I Like Movie");
+expect(makeIntoTitle("i am funny"), "I Am Funny");
+expect(makeIntoTitle("i have a dog"), "I Have A Dog");
+expect(makeIntoTitle("hello banana"), "Hello Banana");
+expect(makeIntoTitle("whats up coconut"), "Whats Up Coconut");
 
 /**
  * -------------------------------------------------------------------
@@ -27,10 +49,8 @@ expect(
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
-    console.log(
-      `⛔️ Expected “${result}” to equal “${value}”`
-    );
+    console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
 }
