b8a5f7e4259bb9e3b0fcc5a581f95c8d3e179f37
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-05-10 11:55:15 -0400

    exercise 2.1 and 2.2

--- workshop/6-2-exercises/exercise-2-1/app.js
+++ workshop/6-2-exercises/exercise-2-1/app.js
@@ -11,3 +11,32 @@
 // - the cells should all be square.
 // - cells should have a class of 'cell'
 // - cells should have and id of 'cell-#'
+
+// grab the board
+let board = document.querySelector('.board');
+
+// amount of cells in a row desired
+const numberOfCell = 6;
+
+// style the board
+board.setAttribute('style', 
+  `width: 600px;
+  height: 600px;
+  grid-template-rows: repeat(${numberOfCell}, 1fr);
+  grid-template-columns: repeat(${numberOfCell}, 1fr);`
+);
+
+// initialize the cells
+let cells;
+
+// loop through the square of numberOfCell 
+for (let i = 0; i < numberOfCell ** 2; i++) {
+  // create the cell
+  cells = document.createElement('div');
+  // add the class of .cell
+  cells.classList.add('cell');
+  // add the id cell-#
+  cells.id = `cell-${i + 1}`;
+  // add the cell to the board
+  board.appendChild(cells);
+}
