1ee8ec119b4b53fdd497aac00a7b01d1b0fbec26
n4dz <nadzamini@gmail.com>
2020-05-10 00:41:04 -0400

    exercise 2-1 done

--- workshop/6-2-exercises/exercise-2-1/app.js
+++ workshop/6-2-exercises/exercise-2-1/app.js
@@ -11,3 +11,22 @@
 // - the cells should all be square.
 // - cells should have a class of 'cell'
 // - cells should have and id of 'cell-#'
+
+/*
+1. Mettre le square à 600px avec css style
+2. for lopp les colonnes et rangés
+3. donner une class au cells
+4. donner id au cells
+*/
+
+let boardSize = document.getElementById("board");
+boardSize.style.height = "600px";
+boardSize.style.width = "600px";
+boardSize.style.gridTemplateRows = "repeat(8,auto)";
+boardSize.style.gridTemplateColumns = "repeat(8,auto)";
+for (let index = 0; index < 8 * 8; index++) {
+  const myCells = document.createElement("div");
+  myCells.classList.add("cell");
+  myCells.setAttribute("id", "cell-" + index);
+  boardSize.appendChild(myCells);
+}
