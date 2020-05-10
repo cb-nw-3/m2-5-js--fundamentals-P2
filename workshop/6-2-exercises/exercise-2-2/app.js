117a3b1ef299eaaf4d7db193fada47f8ac0b0749
Philppe <psalvail@assetscience.com>
2020-05-10 11:28:12 -0400

    slight modification on exercise 6-2

--- workshop/6-2-exercises/exercise-2-2/app.js
+++ workshop/6-2-exercises/exercise-2-2/app.js
@@ -13,24 +13,11 @@
 // - one event listener per input
 
 // If you need extra hints, see somebody... :)
-let gameBoard = document.querySelector("#board");
-gameBoard.setAttribute("style", "height: 600px; width: 600px; display:grid;");
-gameBoard.style.gridTemplateColumns = "repeat(" + numOfCol + ", 1fr)";
-gameBoard.style.gridTemplateRows = "repeat(" + numOfRows + ", 1fr)";
-
-function setUpBoard(numOfRows, numOfCol) {
-  for (let i = 0; i < numOfRows * numOfCol; i++) {
-    let tableCell = document.createElement("div");
-    tableCell.setAttribute("id", "cell-#" + i);
-    tableCell.classList.add("cell");
-    gameBoard.append(tableCell);
-  }
-  rowsValue.value = "";
-  columnsValue.value = "";
-}
-
 let rowsValue = document.querySelector("#rows-input");
 let columnsValue = document.querySelector("#columns-input");
+
+let gameBoard = document.querySelector("#board");
+gameBoard.setAttribute("style", "height: 600px; width: 600px; display:grid;");
 
 columnsValue.addEventListener("blur", function () {
   if (rowsValue.value === "") {
@@ -45,3 +32,16 @@ rowsValue.addEventListener("blur", function () {
     setUpBoard(rowsValue.value, columnsValue.value);
   }
 });
+
+function setUpBoard(numOfRows, numOfCol) {
+  gameBoard.style.gridTemplateColumns = "repeat(" + numOfCol + ", 1fr)";
+  gameBoard.style.gridTemplateRows = "repeat(" + numOfRows + ", 1fr)";
+  for (let i = 0; i < numOfRows * numOfCol; i++) {
+    let tableCell = document.createElement("div");
+    tableCell.setAttribute("id", "cell-#" + i);
+    tableCell.classList.add("cell");
+    gameBoard.append(tableCell);
+  }
+  rowsValue.value = "";
+  columnsValue.value = "";
+}
