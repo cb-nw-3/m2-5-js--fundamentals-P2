6bcb76487f6a403fc40483d0b10fd292f864c189
jenny ma <jenny_lau_8@hotmail.com>
2020-05-10 23:12:40 -0400

    2

--- workshop/6-2-exercises/exercise-1-2/app.js
+++ workshop/6-2-exercises/exercise-1-2/app.js
@@ -20,3 +20,51 @@
 // Because we are now going to work with the opacity and background of the buttons,
 // they need to be set here. So that they are referencable via the the style attribute.
 // initialize the background color for all of the buttons in here. (for loop)
+
+const btnList = document.querySelector('#btn-list');
+
+for (let id = 1; id < 5; id++) {
+    // target the dom element
+    const initButton = document.getElementById(`btn-${id}`);
+
+    // explicitely set the opacity and backgroundColor.
+    initButton.style.opacity = '100';
+    initButton.style.backgroundColor = 'gold';
+}
+
+const toggleColor = (id, color) => {
+    const btnElement = document.getElementById(id);
+    const currentColor = btnElement.style.backgroundColor;
+    if(currentColor === 'gold') {
+        btnElement.style.backgroundColor = `${color}`;
+      } else {
+        btnElement.style.backgroundColor = 'gold';
+      }
+}
+
+const handleClick = (event) => {
+    const btnId = event.target.id;
+    const btnElement = document.getElementById(btnId);
+    switch (btnId) {
+        case 'btn-1':
+            if(btnElement.style.opacity == 100) {
+                btnElement.style.opacity = 0;
+              } else {
+                btnElement.style.opacity = 100;
+              }
+            break;
+        case 'btn-2':
+            toggleColor(btnId, 'crimson');
+            break;
+        case 'btn-3':
+            toggleColor(btnId, 'lightblue');
+            break;
+        case 'btn-4':
+            btnElement.classList.toggle('jitters');
+            break;
+        default:
+            break;
+    }
+}
+
+btnList.addEventListener('click', handleClick);
